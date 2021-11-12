c851a2447ea6d775054d44203bef51c08dce919f
Lucas Zluhan <lszluhan@hotmail.com>
2021-11-11 22:47:07 -0300

    ok

--- public/api.js
+++ public/api.js
@@ -3,7 +3,7 @@ const api = axios.create({ baseURL: 'https://api-growdevers-lucaszluhan.herokuap
 api.get('/')
    .then((result) => {
       console.log(result.data.data);
-      document.querySelector('#lista').innerHTML = `ola`;
+      document.querySelector('#lista').innerHTML = `<li>ola</li>`;
    })
    .catch((err) => {
       console.log('erro');
