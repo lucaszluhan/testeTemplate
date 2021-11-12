8c48cc27d1c4ee62726ee059ef62dd1f69c212df
Lucas Zluhan <lszluhan@hotmail.com>
2021-11-11 22:21:50 -0300

    att

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,4 @@ const app = express();
 app.use(express.json());
 app.use(express.static(__dirname + '/../public'));
 
-app.listen(8081, () => console.log('Server is running...'));
+app.listen(process.env.PORT || 8081, () => console.log('Server is running...'));
